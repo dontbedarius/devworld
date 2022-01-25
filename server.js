7ab8fb92f9a38327af6090d224fe87a840a71b51
Darius <darius@dontbedarius.com>
2022-01-24 22:51:08 -0600

    Prepared for Deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 // Connect Database
@@ -10,16 +11,22 @@ connectDB();
 //Init Middleware(body-parser)
 app.use(express.json({ extended: false }));
 
-// app.get('/', (req, res) => res.send('API Running'));
-
 // All Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+//Static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, function () {
-  console.log('Server is active on port 5000');
-});
+app.listen(PORT, () => console.log(`Server is active on port ${PORT}`));
